bf51355a828b2b8252d72293041ec08a9707e4e7
Tapan <tapansonak786@gmail.com>
2024-02-05 02:38:18 +0530

    compressing data

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const server = jsonServer.create();
 const router = jsonServer.router("./data/questions.json");
 const middlewares = jsonServer.defaults();
 const cors = require("cors");
+const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
 
+// Add compression middleware
+server.use(compression());
 server.use(middlewares);
 server.use(router);
 server.use(cors());
